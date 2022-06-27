2cc51dd68060047f4cb1113693dc36c3ab46ba60
Crocoya <yassine.tababi@outlook.com>
2022-06-28 00:21:00 +0200

    add style articles

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,21 +1,20 @@
 import Link from 'next/link';
 import { v4 as uuidv4 } from 'uuid';
+import styled from 'styled-components';
 
 const Blog = (props: any) => {
   return (
-    <div>
+    <BlogContainer>
       {props.articles.map((item: any) => (
-        <div key={uuidv4()}>
-          <div>
-            <h5>{item.title}</h5>
-            <p>{item.body.slice(0, 20) + '...'}</p>
-            <Link href={`/blog/${item.id.toString()}`}>
-              <a>Read this article</a>
-            </Link>
-          </div>
-        </div>
+        <BlogContent key={uuidv4()}>
+          <h5>{item.title}</h5>
+          <p>{item.body.slice(0, 20) + '...'}</p>
+          <Link href={`/blog/${item.id.toString()}`}>
+            <a>Read this article</a>
+          </Link>
+        </BlogContent>
       ))}
-    </div>
+    </BlogContainer>
   );
 };
 
@@ -31,3 +30,38 @@ export async function getStaticProps() {
     },
   };
 }
+
+const BlogContainer = styled.section`
+  width: 100%;
+  margin: 0 auto;
+  padding: 3rem 1.5rem;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  grid-column-gap: 5px;
+  grid-row-gap: 5px;
+`;
+
+const BlogContent = styled.div`
+  background-color: gray;
+  height: 100%;
+  padding: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  h5 {
+    color: blue;
+    text-transform: uppercase;
+  }
+
+  p {
+    color: yellow;
+  }
+
+  a {
+    background-color: white;
+    padding: 5px;
+    margin: 5px 0;
+  }
+`;
